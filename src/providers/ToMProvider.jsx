240e68e93ace6a4598994f22ef4500ea675c0195
Klara Frommelin <freakyfrommelin@outlook.com>
2025-07-03 15:02:45 +0200

    Storing token for movie-requests

--- src/providers/ToMProvider.jsx
+++ src/providers/ToMProvider.jsx
@@ -3,7 +3,14 @@ export const ToMContext = createContext()
 
 const ToMProvider = (props) => {
   const [token, setToken] = useState('')
-  const changeToken = (val) => {setToken(val)}
+  const changeToken = (val) => {
+    if(val != ''){
+      localStorage.setItem('user', val)
+    } else {
+      localStorage.removeItem('user')
+    }
+    setToken(val)
+  }
 
   return (
     <ToMContext.Provider value = {{token, changeToken}}>
