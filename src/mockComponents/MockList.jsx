c4cb804307d0335c9050781b7b32c194193897eb
Klara Frommelin <freakyfrommelin@outlook.com>
2025-07-06 12:52:57 +0200

    Setting up for mocked tests

--- src/mockComponents/MockList.jsx
+++ src/mockComponents/MockList.jsx
@@ -3,11 +3,46 @@ import { useState } from 'react'
 const MockList = () => {
   const [movies,setMovies] = useState([])
   const [error,setError] = useState('')
+  const [token,setToken] = useState('')
+
+  const loginAndGetMovies = async () => {
+    try{
+      //login and get token
+      const response = await fetch('https://tokenservice-jwt-2025.fly.dev/token-service/v1/request-token',{
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          'username': 'klara@example.com',
+          'password': 'havefuncodeing123'
+        })
+      })
+
+      if (!response.ok) throw new Error('Det blev något fel vid inloggningen')
+
+      const jwt = await response.text()
+      setToken(jwt)
+
+      //fetch list of saved movies
+      const movieResult = await fetch('https://tokenservice-jwt-2025.fly.dev/movies',{
+        headers:{
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      if(!movieResult.ok) throw new Error('Det blev fel vid hämtning av filmer')
+      const movieData = await movieResult.json()
+     setMovies(movieData)
+    } catch(err){
+      setError(err)
+    }
+  }
 
   return (
     <div>
       {error && <h3>{error}</h3>}
-      <button>Logga in och hämta filmer</button>
+      <button onClick={loginAndGetMovies}>Logga in och hämta filmer</button>
       <ul>
         {movies.map((movie, i) => (
           <li key={i}>
---
+++ src/test/mocks/handlers.js
@@ -0,0 +1,58 @@
+import { http, HttpResponse } from "msw";
+
+export const handlers = [
+  http.post('URL', async ({ request }) => {
+    //Collecting login details from component
+    const {username,password} = await request.json()
+
+    if(username == 'Klara' && password == 'havefuncodeing123'){
+      return new HttpResponse('mocked-jwt', {
+        status: 200,
+        headers: {
+          'Content-Type': 'text/plain'
+        }
+      })
+    } else {
+      return HttpResponse.json(
+        {message: 'Unauthorized'},
+        {status: 401}
+      )
+    }
+  }),
+
+  http.get('URL', ({ request }) => {
+    //get auth header
+    const authHeader = request.headers.get('Authorization')
+
+    if(authHeader === 'Bearer mocked-jwt'){
+      return HttpResponse.json([
+        {    
+          "title": "The life of John Doe",
+          "director": "John Doe Thats his name",
+          "description": "Describes the extraordinary life of John Doe.",
+          "productionYear": 2000,
+          "id": 1
+        },
+        {    
+          "title": "Avatar",
+          "director": "James Cameron",
+          "description": "Blue spacemonkeys fight humans to defend their home-planet.",
+          "productionYear": 2005,
+          "id": 2
+        },
+        {    
+          "title": "The Hunchback of Notredame",
+          "director": "Walt Disney",
+          "description": "Inprioned deformed gypzy helps another gypzy escape Paris's insane leader.",
+          "productionYear": 1996,
+          "id": 3
+        }
+      ])
+    } else {
+      return HttpResponse.json(
+        {message: 'Forbidden'},
+        {status: 403}
+      )
+    }
+  })
+]
